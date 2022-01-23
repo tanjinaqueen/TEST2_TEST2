7f30fe067c2ae9861762a308e7f90904288e6fb0
test <you@mail.com>
2022-01-23 00:04:57 -0500

    pushing first time

---
+++ src/main/java/test_shetu1/tanjina1/App.java
@@ -0,0 +1,13 @@
+package test_shetu1.tanjina1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
